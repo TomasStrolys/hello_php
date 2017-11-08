05145d4a85c45e98314b1a474fc75a61171f9ed8
TomasStrolys <33262525+TomasStrolys@users.noreply.github.com>
2017-11-08 20:52:56 +0200

    Variables

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$string = "Labas";
+$number = 156;
+$real = 15.54;
+$boolean = true;
+$array =["lt", "en", "lv"];
+$object = (object)[
+	               "key" => "value",
+                   "key2" => "1value",
+                   "key3" => "2value",
+                   "key4" => "3value",
+                   "key5" => "4value",
+                   ];
+
+//$object = (object ["lt", "en", "lv"]);
+
+// var_dump($string);
+// var_dump($number);
+// var_dump($real);
+// var_dump($boolean);
+// var_dump($array);
+// var_dump($object);
+
+var_dump($array[0]);
+var_dump($object->key2);
+
+
+
+//echo "Hello";
+
+
+
+
+
